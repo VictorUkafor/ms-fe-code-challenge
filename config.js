ba59614490c3de53ae165a8843a489bdeb029f7f
Victor Ukafor <victorukafor@gmail.com>
2019-01-05 12:19:02 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumerKey: process.env.YOUR_TWITTER_API_CONSUMER_KEY,
+  consumerSecret: process.env.YOUR_TWITTER_API_CONSUMER_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+  port: process.env.PORT
+}
