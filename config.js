c6c158f4f7ecdcd3395fd3b4728f52465c4e6bc2
Victor Ukafor <victorukafor@gmail.com>
2019-01-05 18:29:11 +0100

    add twitter api key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumerKey: process.env.YOUR_TWITTER_API_CONSUMER_KEY,
-  consumerSecret: process.env.YOUR_TWITTER_API_CONSUMER_SECRET,
+  consumerKey: process.env.TWITTER_API_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_API_CONSUMER_SECRET,
   accessToken: process.env.ACCESS_TOKEN,
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
   port: process.env.PORT
