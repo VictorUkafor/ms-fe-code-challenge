7355dba427c5a7530006b2bc1ee553cb30071d45
Victor Ukafor <victorukafor@gmail.com>
2019-01-09 14:41:49 +0100

    organise code into MVC pattern

--- routes.js
+++ routes.js
@@ -1,16 +1,9 @@
 import express from 'express';
-import fetchTweets from './lib/fetchTweets';
+import { fetchTweets } from './controller';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  const makeschoolUrl = 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=makeschool';
-  const newsycombinatorUrl = 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=newsycombinator';
-  const ycombinatorUrl = 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=ycombinator';
-  const urls = [makeschoolUrl, newsycombinatorUrl, ycombinatorUrl];
-
-  return fetchTweets(urls, res);
-});
+router.get('/', fetchTweets);
 
 
 export default router;
