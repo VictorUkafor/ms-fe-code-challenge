86f8ecd41071b41e875f33871614010b890431ad
victor ukafor <13146390+VictorUkafor@users.noreply.github.com>
2019-01-14 00:18:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', router);
-app.listen(port);
+server.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
 
 export default app;
